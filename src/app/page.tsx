39b1ae9da964c77a5f60d9347bf175e2532fc5d8
aHasnat1997 <hasnat56703@gmail.com>
2024-02-04 19:25:27 +0600

    ADD: Home sections Hero, HealthyCard, TopProducts, Banner, NewArrivals and ProductCard for products

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,18 @@
 import Image from "next/image";
+import Hero from "./home/Hero";
+import HealthyCard from "./home/HealthyCard";
+import TopProducts from "./home/TopProducts";
+import Banner from "./home/Banner";
+import NewArrivals from "./home/NewArrivals";
 
 export default function Home() {
   return (
     <main>
-      <h1>Next app...</h1>
+      <Hero />
+      <HealthyCard />
+      <TopProducts />
+      <Banner />
+      <NewArrivals />
     </main>
   );
 }
